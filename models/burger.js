84f0afa469fb964910f9e919392d953eef4a2891
Leandra Turner <calliet0715@gmail.com>
2020-03-15 15:10:53 -0500

    Updating the three  orms with correct function names:

--- models/burger.js
+++ models/burger.js
@@ -4,18 +4,18 @@ var orm = require("../config/orm.js");
 var burger = {
   selectAll: function(cb) {
     //burgers = table name
-    orm.all("burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   insertOne: function(cols, vals, cb) {//column name, values
-    orm.create("burgers", cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   updateOne: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {//updating the state of the burger
+    orm.updateOne("burgers", objColVals, condition, function(res) {//updating the state of the burger
       //condition =
       cb(res);
     });
