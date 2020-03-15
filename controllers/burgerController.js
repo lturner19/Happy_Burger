eb6a1e01f6764cf93fcb83d4d2e5abc09d446dce
Leandra Turner <calliet0715@gmail.com>
2020-03-14 21:21:30 -0500

    Updating routes with 'burgers' and removal of delete route

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,3 +1,6 @@
+//This controller file sets up the routes
+
+
 var express = require("express");
 
 var router = express.Router();
@@ -6,7 +9,9 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {// homepage loaded (select all data from database)
+router.get("/burgers", function(req, res) {
+  // homepage loaded (select all data from database)
+
   burger.all(function(data) {
     var hbsObject = {
       cats: data
@@ -16,23 +21,23 @@ router.get("/", function(req, res) {// homepage loaded (select all data from dat
   });
 });
 
-router.post("/api/cats", function(req, res) {//create new burger(expecting name and something from the body from the front end)
-  cat.create([//executes 
-    "name", "sleepy"//columns from database
+router.post("/api/burgers", function(req, res) {//create new burger(expecting name and something from the body from the front end)
+  burger.create([
+    "burger_name", "devour"//columns from database
   ], [
-    req.body.name, req.body.sleepy//something coming from the front end/ found in public/js/cats.js (var newCat = {name: $(#ca).val().trim() sleep: $("[name = sleepy]")})
+    req.body.name, req.body.devour//something coming from the front end/ found in public/js/cats.js (var newCat = {name: $(#ca).val().trim() sleep: $("[name = sleepy]")})
   ], function(result) {//my asynchrouous or use the callback method
     // Send back the ID of the new quote
     res.json({ id: result.insertId });//response sending to user
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {//updating 
+router.put("/api/burgers/:id", function(req, res) {//updating 
 
   console.log("condition", condition);
 
-  cat.update({//updating a burger
-    sleepy: req.body.sleepy
+  burger.update({//updating a burger
+    devour: req.body.devour
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -43,18 +48,6 @@ router.put("/api/cats/:id", function(req, res) {//updating
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {//do not have to delete any burgers
-  var condition = "id = " + req.params.id;//actual condition being set in where clause
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
