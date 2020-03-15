70c65e67d5b4ff37806186f20697509e7d326189
Leandra Turner <calliet0715@gmail.com>
2020-03-15 09:06:12 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,7 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
-// Make connection.
+// Make connection. 
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
