0aa961dd05cde1492a8789d3cd5e17c7ce7023e4
Orchid23 <calliet0715@gmail.com>
2020-03-16 06:44:44 -0500

    Removal of false as that is the default in table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name, devour) VALUES ("Baconator", false);
+INSERT INTO burgers (burger_name) VALUES ("Baconator");
 
 INSERT INTO burgers (burger_name, devour) VALUE ("Big Mac", true);
 
-INSERT INTO burgers (burger_name, devour) VALUE ("Whopper Jr", false);
+INSERT INTO burgers (burger_name) VALUE ("Whopper Jr");
