dc3907822b5eaac1676a6bf4233666b9cd6a3bd1
Leandra Turner <calliet0715@gmail.com>
2020-03-15 22:12:22 -0500

    Removal of devour value from var newBurger

--- public/assets/javaBurger.js
+++ public/assets/javaBurger.js
@@ -1,11 +1,16 @@
 $(function () {
     $(".change-state").on("click", function (event) {
+        //creating variable for burger id
         var id = $(this).data("id");
+        //creating variable for devour state of burger
         var newState = $(this).attr("data-newState");
+
         console.log(newState)
+        //
         var newEatenState = {
             devour: newState
         };
+        console.log(newEatenState);
 
         //sending a put request to the backend (updating burger page with new burger user creates)
         $.ajax("/api/burgers/" + id, {
@@ -19,13 +24,17 @@ $(function () {
         );
     });
     //information from the front end (index.handlebars)
-    $(".create-form").on("submit", function (event) {
+    $(".create-form").on("submit", function(event) {
         event.preventDefault();
-        alert("Submitted successfully")
+        
+        console.log ("Submitted successfully") //used to determine if submit is working
+
+        //creating a variable to hold the user's burger
         var newBurger = {
-            name: $("#burger").val().trim(),
-            devour: $("[name = devoured]:checked").val().trim()
+           name: $("#burger").val().trim()
         };
+        console.log(newBurger);
+
         //sending the POST response for new burger
         $.ajax("/api/burgers", {
             type:"POST",
