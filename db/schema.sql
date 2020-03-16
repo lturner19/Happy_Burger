2a89b9b9e3cee1a27f1dce7dd3227c5263247939
Orchid23 <calliet0715@gmail.com>
2020-03-16 07:25:46 -0500

    Updating database to include an s = burgers_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 ### Schema
-DROP DATABASE IF EXISTS burger_db;
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers
 (
